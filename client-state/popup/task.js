5767cef9e34edd432aa9e706e144489d84b33298
stanislavsamo <f11samokhin@gmail.com>
2023-12-13 01:28:03 +0500

    commit popup 3

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,11 +1,13 @@
-const modal = document.getElementById('subscribe-modal');
-modal.classList.add('modal_active');
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById('subscribe-modal');
+  modal.classList.add('modal_active');
 
-if (document.cookie.includes('modal__close=yes')) {
-	modal.classList.remove('modal_active');
-}
+  if (document.cookie.includes('modal__close=yes')) {
+    modal.classList.remove('modal_active');
+  }
 
-document.querySelector('.modal__close').addEventListener('click', function() {
-	document.cookie = 'modal__close=yes; path=/';
-	modal.classList.remove('modal_active');
+  document.querySelector('.modal__close').addEventListener('click', function() {
+    document.cookie = 'modal__close=yes; path=/';
+    modal.classList.remove('modal_active');
+  });
 });
